05618a47398028dd0756a3c2a17e9ae33e9be5f0
kujebe <kujebeseun@gmsil.com>
2024-04-29 10:41:08 +0100

    Add checkpoints

--- start2.sh
+++ start2.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-if [ ! -d "Fooocus" ]; then
-  git clone https://github.com/lllyasviel/Fooocus.git
+if [ ! -d "DeFooocus" ]; then
+  git clone https://github.com/ehristoforu/DeFooocus.git
 fi
 
-cd Fooocus
+cd DeFooocus
 git pull
 
-if [ ! -L ~/.conda/envs/fooocus ]; then
-    ln -s /tmp/fooocus ~/.conda/envs/
+if [ ! -L ~/.conda/envs/DeFooocus ]; then
+    ln -s /tmp/DeFooocus ~/.conda/envs/
 fi
 
 eval "$(conda shell.bash hook)"
 
-if [ ! -d /tmp/fooocus ]; then
-    mkdir /tmp/fooocus
+if [ ! -d /tmp/DeFooocus ]; then
+    mkdir /tmp/DeFooocus
     conda env create -f environment.yaml
-    conda activate fooocus
+    conda activate DeFooocus
     pip install -r requirements_versions.txt
     pip install torch torchvision --force-reinstall --index-url https://download.pytorch.org/whl/cu117
     conda install glib -y
@@ -26,6 +26,13 @@ fi
 # Setup the path for model checkpoints
 current_folder=$(pwd)
 model_folder=${current_folder}/models/checkpoints-real-folder
+# Download and save checkpoint
+wget  -O ${current_folder}/models/checkpoints/juggernautXL_juggernautxX.safetensors https://civitai.com/api/download/models/456194
+wget  -O ${current_folder}/models/checkpoints/realisticVisionV60B1_v60B1VAE.safetensors https://civitai.com/api/download/models/245598
+
+# Download and save LoRAs
+# !wget  -O ${current_folder}/models/loras/Leather_armor.safetensors https://civitai.com/api/download/models/454069
+# !wget  -O ${current_folder}/models/checkpoints/20ModelPosesAndPrompts_v20.safetensors https://civitai.com/api/download/models/397649
 if [ ! -e config.txt ]; then
   json_data="{ \"path_checkpoints\": \"$model_folder\" }"
   echo "$json_data" > config.txt
@@ -41,13 +48,33 @@ if [ ! -L models/checkpoints ]; then
     ln -s models/checkpoints-real-folder models/checkpoints
 fi
 
-# Activate the fooocus environment
-conda activate fooocus
+# Activate the DeFooocus environment
+conda activate DeFooocus
 cd ..
 
-# Run Python script in the background
-python Fooocus/entry_with_update.py --always-high-vram &
 
+# Set the image theme
+image_theme="dark"  # Allowed values: "dark", "light"
+
+# Set the image preset
+image_preset="default"  # Allowed values: "default", "realistic", "anime", "lcm", "sai", "turbo", "lighting"
+
+# Set the advanced arguments
+advanced_args="--share --attention-split --always-high-vram --disable-offload-from-vram --all-in-fp16"
+
+# Construct the full argument string
+if [ "$image_preset" != "default" ]; then
+  args="$advanced_args --theme $image_theme --preset $image_preset"
+else
+  args="$advanced_args --theme $image_theme"
+fi
+
+# Run the Python script in the background
+echo "Running DeFooocus/entry_with_update.py with the following arguments: $args"
+python DeFooocus/entry_with_update.py $args &
+
+# Wait for 120 seconds
+echo "Waiting for 120 seconds..."
 sleep 120
 
 # Run cloudflared tunnel
